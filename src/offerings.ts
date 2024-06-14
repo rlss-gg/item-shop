f53053e71697e19c769e2b5eb542029970cfc99e
Finley Sherwood <me@itsf.in>
2024-06-14 19:34:21 +1000

    Fix font name after testing

--- src/offerings.ts
+++ src/offerings.ts
@@ -11,6 +11,13 @@ const date: DisplayDate = DisplayDate.fromUnixEpoch(Date.now())
 /**
  * The offerings in the current item shop.
  */
-const offerings: OfferingProps[] = []
+const offerings: OfferingProps[] = [
+  NewItem(Items.banners.SolarSystem, 400, 1),
+  Item(Items.decals.Mantis.Critters, 800, 1, "Purple"),
+  Item(Items.wheels.Barouche, 1200, 1, "Crimson"),
+  Item(Items.rocketBoosts.MagicMissile, 1800, 5, "Orange"),
+  Item(Items.wheels.Ultralex, 2400, 5, "Lime"),
+  Item(Items.bodies.BreakoutS, 5000, 5, "Saffron")
+]
 
 export { date, offerings }
